e676b27a7d86fb99ffaf9376199660125428c167
karimmoustamid <moustamid.teck@gmail.com>
2025-01-24 15:10:15 -0600

    Add initial implementation of RemoveDuplicates method
    Created a new class `RemoveDuplicatesFromSortedArray` under the `Solutions.Eazy.Array` namespace. Added a stub method `RemoveDuplicates` that currently returns 0, laying the groundwork for future functionality.

---
+++ Solutions/Eazy/Array/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,10 @@
+namespace Solutions.Eazy.Array
+{
+    public static class RemoveDuplicatesFromSortedArray
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
